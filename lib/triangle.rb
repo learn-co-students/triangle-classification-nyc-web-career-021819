795fb3a74b2a999386e5f630723d6d4e48cd9330
tchang9 <tlchang@gmail.com>
2019-03-01 15:25:12 -0500

    triagle errors

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @kind = kind
+  end
+
+  def valid
+    a + b > c and a + c > b and b + c > a
+  end
+
+
+  def kind
+    if !self.valid
+      raise TriangleError 
+
+    elsif a == b and b == c
+      return :equilateral
+
+    elsif a != b and b !=c and c != a
+      return :scalene
+    
+    else
+      return :isosceles
+    end
+
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
